c04dd8994baf7b17224a3a5927aef44855ccf8bb
JuanaSM <juana.sipriano@uabc.edu.mx>
2025-03-31 21:08:16 -0700

    mi proyecto sprint 7

---
+++ app.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+car_data = pd.read_csv(
+    "/Users/juanasipriano/Documents/my-proyecto-final-7/vehicles_us.csv")
+hist_button = st.button('Construir histograma')
+scatter_button = st.button('Construir gráfico de dispersión')
+if hist_button:
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    fig = px.histogram(car_data, x="odometer")
+    st.plotly_chart(fig, use_container_width=True)
+
+if scatter_button:
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    fig = px.scatter(car_data, x="odometer", y="price")
+    st.plotly_chart(fig, use_container_width=True)
